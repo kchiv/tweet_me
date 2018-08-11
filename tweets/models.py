3384db3959de609144d76339f058bc4cebf1c14b
Kyle Chivers <kchiv@kyles-mbp.lan>
2018-08-11 15:18:04 -0700

    added foreign key to model

--- tweets/models.py
+++ tweets/models.py
@@ -1,8 +1,10 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
 
 class Tweet(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
 	content = models.CharField(max_length=140)
 	timestamp = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
