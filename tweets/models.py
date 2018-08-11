08feafc7bad16dec5722538f8da24f2c061a4818
Kyle Chivers <kchiv@kyles-mbp.lan>
2018-08-11 14:32:21 -0700

    updated tweet model

--- tweets/models.py
+++ tweets/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Tweet(models.Model):
-	content = models.TextField()
+	content = models.CharField(max_length=140)
+	timestamp = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
 
 	def __str__(self):
 		return str(self.content)
