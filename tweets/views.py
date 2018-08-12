87a0956640805bb69fc937c0815bc39e06c332e3
Kyle Chivers <kchiv@kyles-mbp.lan>
2018-08-11 20:39:14 -0700

    added get 404 function

--- tweets/views.py
+++ tweets/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Tweet
 
@@ -6,7 +6,7 @@ from .models import Tweet
 
 
 def tweet_detail_view(request, pk):
-	obj = Tweet.objects.get(pk=pk) #GET from database
+	obj = get_object_or_404(Tweet, pk=pk) #GET from database
 	context = {
 		'object': obj
 	}
