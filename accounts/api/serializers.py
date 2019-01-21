ca53b4fb7f6ffed70e4abb381eec3c5ccc28550c
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-01-21 12:55:06 -0800

    created accounts app

---
+++ accounts/api/serializers.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+class UserDisplaySerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = [
+			'username',
+			'first_name',
+			'last_name',
+		]
