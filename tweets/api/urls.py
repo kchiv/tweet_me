250e412ad860a136320997a0abef3f0e9fdc6048
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-01-27 18:04:56 -0800

    url routing for create api view

--- tweets/api/urls.py
+++ tweets/api/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 
 from .views import (
+	TweetCreateAPIView,
 	TweetListAPIView
 	)
 
 urlpatterns = [
 	url(r'^$', TweetListAPIView.as_view(), name='list'), #/api/tweet/
+	url(r'^create/$', TweetCreateAPIView.as_view(), name='create')
 ]
