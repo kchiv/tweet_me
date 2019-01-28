ec7d5167306bec266b82d44bbc5fe00377510f5b
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-01-27 21:59:26 -0800

    changed order of tweets

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -17,7 +17,7 @@ class TweetListAPIView(generics.ListAPIView):
 	serializer_class = TweetModelSerializer
 
 	def get_queryset(self, *args, **kwargs):
-		queryset = Tweet.objects.all()
+		queryset = Tweet.objects.all().order_by('-timestamp')
 		query = self.request.GET.get('q', None)
 		if query is not None:
 			queryset = queryset.filter(
