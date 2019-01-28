6861f66713f6143214c97edc960bece682040441
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-01-27 18:04:32 -0800

    made user read only

--- tweets/api/serializers.py
+++ tweets/api/serializers.py
@@ -4,7 +4,7 @@ from accounts.api.serializers import UserDisplaySerializer
 from tweets.models import Tweet # alternate import - from ..models import Tweet
 
 class TweetModelSerializer(serializers.ModelSerializer):
-	user = UserDisplaySerializer()
+	user = UserDisplaySerializer(read_only=True)
 	class Meta:
 		model = Tweet
 		fields = [
