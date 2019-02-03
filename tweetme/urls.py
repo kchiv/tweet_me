821074b17c0a74f33d55cc5f7e29854771a9e33c
Kyle Chivers <kchiv@Kyles-MacBook-Pro.local>
2019-02-03 14:38:18 -0800

    updated routing for account to be off root

--- tweetme/urls.py
+++ tweetme/urls.py
@@ -24,9 +24,9 @@ from .views import home
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', tweet_list_view, name='home'),
-    url(r'^profiles/', include('accounts.urls', namespace='profiles')),
     url(r'^tweet/', include('tweets.urls', namespace='tweet')),
     url(r'^api/tweet/', include('tweets.api.urls', namespace='tweet-api')),
+    url(r'^', include('accounts.urls', namespace='profiles')),
 ]
 
 if settings.DEBUG:
