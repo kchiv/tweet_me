42387bfef5e7aa45d152d694321baf548bfb3557
Kyle Chivers <kchiv@Kyles-MacBook-Pro.local>
2019-02-03 14:33:58 -0800

    pass user detail through api

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,13 +1,20 @@
 from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
+
 from rest_framework import serializers
 
 User = get_user_model()
 
 class UserDisplaySerializer(serializers.ModelSerializer):
+	url = serializers.SerializerMethodField()
 	class Meta:
 		model = User
 		fields = [
 			'username',
 			'first_name',
 			'last_name',
+			'url',
 		]
+
+	def get_url(self, obj):
+		return reverse_lazy('profiles:detail', kwargs={'username': obj.username})
