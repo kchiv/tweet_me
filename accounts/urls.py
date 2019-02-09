7c1fea78929291084f57f665875ae266404165c7
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-02-08 22:20:05 -0800

    routing for follow view

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 
 from .views import (
-        UserDetailView
+        UserDetailView,
+        UserFollowView
     )
 
 urlpatterns = [
     url(r'^(?P<username>[\w.@+-]+)/$', UserDetailView.as_view(), name='detail'),
+    url(r'^(?P<username>[\w.@+-]+)/follow/$', UserFollowView.as_view(), name='follow'),
 ]
