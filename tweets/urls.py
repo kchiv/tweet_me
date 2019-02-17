bef091a3aa69d85492ad338372b31b2200359cd8
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-02-17 15:23:34 -0800

    routing for retweets

--- tweets/urls.py
+++ tweets/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from django.views.generic.base import RedirectView
 from .views import (
+        RetweetView,
         tweet_detail_view,
         tweet_list_view,
         TweetCreateView,
@@ -13,6 +14,7 @@ urlpatterns = [
     url(r'^$', RedirectView.as_view(url='/')),
     url(r'^search/$', tweet_list_view, name='list'),
     url(r'^(?P<pk>\d+)/$', tweet_detail_view, name='detail'),
+    url(r'^(?P<pk>\d+)/retweet/$', RetweetView.as_view(), name='retweet'),
     url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),
     url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete'),
     url(r'^create/$', TweetCreateView.as_view(), name='create')
