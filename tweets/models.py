8a1729d8fa3f89a118a35513fc29c6f8a66c63cd
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-02-17 15:22:51 -0800

    model manager for retweets

--- tweets/models.py
+++ tweets/models.py
@@ -6,11 +6,28 @@ from .validators import validate_content
 
 # Create your models here.
 
+class TweetManager(models.Manager):
+	def retweet(self, user, parent_obj):
+		if parent_obj.parent:
+			og_parent = parent_obj.parent
+		else:
+			og_parent = parent_obj
+		obj = self.model(
+				parent = og_parent,
+				user = user,
+				content = parent_obj.content,
+			)
+		obj.save()
+		return obj
+
 class Tweet(models.Model):
+	parent = models.ForeignKey('self', blank=True, null=True)
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
 	content = models.CharField(max_length=140, validators=[validate_content])
 	timestamp = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
+
+	objects = TweetManager()
 
 	def __str__(self):
 		return str(self.content)
