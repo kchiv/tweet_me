b5be524732638c19ecade2c75ecfc20c9a9ffe36
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-02-19 20:50:01 -0800

    url routing for retweet

--- tweets/api/urls.py
+++ tweets/api/urls.py
@@ -3,11 +3,13 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 
 from .views import (
+	RetweetAPIView,
 	TweetCreateAPIView,
 	TweetListAPIView
 	)
 
 urlpatterns = [
 	url(r'^$', TweetListAPIView.as_view(), name='list'), #/api/tweet/
-	url(r'^create/$', TweetCreateAPIView.as_view(), name='create')
+	url(r'^create/$', TweetCreateAPIView.as_view(), name='create'),
+	url(r'^(?P<pk>\d+)/retweet/$', RetweetAPIView.as_view(), name='retweet')
 ]
