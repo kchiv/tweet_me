0e79f4dced0b7b97b9356f04d7f9153093f60d1a
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-02-24 20:55:44 -0800

    api routing for like toggle

--- tweets/api/urls.py
+++ tweets/api/urls.py
@@ -3,13 +3,15 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 
 from .views import (
+	LikeToggleAPIView,
 	RetweetAPIView,
 	TweetCreateAPIView,
-	TweetListAPIView
+	TweetListAPIView,
 	)
 
 urlpatterns = [
 	url(r'^$', TweetListAPIView.as_view(), name='list'), #/api/tweet/
 	url(r'^create/$', TweetCreateAPIView.as_view(), name='create'),
-	url(r'^(?P<pk>\d+)/retweet/$', RetweetAPIView.as_view(), name='retweet')
+	url(r'^(?P<pk>\d+)/like/$', LikeToggleAPIView.as_view(), name='like-toggle'),
+	url(r'^(?P<pk>\d+)/retweet/$', RetweetAPIView.as_view(), name='retweet'),
 ]
