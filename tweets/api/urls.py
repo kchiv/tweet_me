89e8d602581381f174766ff5ba5e366ff41b5860
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-03-06 20:21:49 -0800

    added tweet detail view routing

--- tweets/api/urls.py
+++ tweets/api/urls.py
@@ -7,11 +7,13 @@ from .views import (
 	RetweetAPIView,
 	TweetCreateAPIView,
 	TweetListAPIView,
+	TweetDetailAPIView,
 	)
 
 urlpatterns = [
 	url(r'^$', TweetListAPIView.as_view(), name='list'), #/api/tweet/
 	url(r'^create/$', TweetCreateAPIView.as_view(), name='create'),
+	url(r'^(?P<pk>\d+)/$', TweetDetailAPIView.as_view(), name='detail'),
 	url(r'^(?P<pk>\d+)/like/$', LikeToggleAPIView.as_view(), name='like-toggle'),
 	url(r'^(?P<pk>\d+)/retweet/$', RetweetAPIView.as_view(), name='retweet'),
 ]
